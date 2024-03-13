00ed32ccd695c463f238666b52762f88a3dd6b93
weichengwillie <weicheng911011@gmail.com>
2024-03-13 14:43:14 +0800

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,14 @@
+x = 3
+if x < 3:
+    print("x小於3")
+elif x > 4:
+    print("x大於3")
+else:
+    print("一樣誒")
+    
+    
+n1 = int(input("輸入今年年度（西元）"))   
+n2 = int(input("輸入您的出生年（西元）"))
+n3 = n1 - n2
+print("您今年 %d 歲" %n3)
+
